fe5e3796d243e5a47970cfa684e8bec82692acc8
jDudek <jakub.dudek94@gmail.com>
2023-08-02 12:19:37 +0200

    New branch 3.2.0_MOCKK_OK -> InjectMock (mockk) for clock bean works properly

--- src/main/kotlin/org/acme/GreetingResource.kt
+++ src/main/kotlin/org/acme/GreetingResource.kt
@@ -1,24 +1,19 @@
 package org.acme
 
-import io.smallrye.mutiny.Uni
 import jakarta.ws.rs.GET
 import jakarta.ws.rs.Path
 import jakarta.ws.rs.Produces
 import jakarta.ws.rs.core.MediaType.APPLICATION_JSON
-import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedAsyncClient
-import software.amazon.awssdk.enhanced.dynamodb.TableSchema
+import java.time.Clock
 
 @Produces(APPLICATION_JSON)
 @Path("/hello")
 class GreetingResource(
-        dynamoClient: DynamoDbEnhancedAsyncClient
+        private val clock: Clock
 ) {
 
-    private val tableSchema = TableSchema.fromClass(Greeting::class.java)
-    private val table = dynamoClient.table("Table", tableSchema)
-
     @GET
-    fun hello(): Uni<Greeting> {
-        return Uni.createFrom().completionStage { table.updateItem(Greeting("1")) }
+    fun hello(): String {
+        return clock.instant().toString()
     }
 }
--- src/test/kotlin/org/acme/GreetingResourceTest.kt
+++ src/test/kotlin/org/acme/GreetingResourceTest.kt
@@ -2,25 +2,18 @@ package org.acme
 
 import io.mockk.every
 import io.quarkiverse.test.junit.mockk.InjectMock
-
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.module.kotlin.extensions.Extract
 import io.restassured.module.kotlin.extensions.Then
 import io.restassured.module.kotlin.extensions.When
-import jakarta.inject.Inject
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import software.amazon.awssdk.services.dynamodb.DynamoDbClient
-import software.amazon.awssdk.services.dynamodb.model.*
 import java.time.Clock
 import java.time.OffsetDateTime
 import java.time.ZoneOffset
 
 @QuarkusTest
 class GreetingResourceTest {
-
-    @Inject
-    lateinit var client: DynamoDbClient
 
     @InjectMock
     private lateinit var clock: Clock
@@ -31,30 +24,6 @@ class GreetingResourceTest {
     fun setUp() {
         every { clock.zone } returns timestamp.offset
         every { clock.instant() } returns timestamp.toInstant()
-
-        client.createTable(
-                CreateTableRequest.builder()
-                        .attributeDefinitions(
-                                AttributeDefinition.builder()
-                                        .attributeName("id")
-                                        .attributeType(ScalarAttributeType.S)
-                                        .build()
-                        )
-                        .keySchema(
-                                KeySchemaElement.builder()
-                                        .attributeName("id")
-                                        .keyType(KeyType.HASH)
-                                        .build()
-                        )
-                        .provisionedThroughput(
-                                ProvisionedThroughput.builder()
-                                        .readCapacityUnits(10)
-                                        .writeCapacityUnits(10)
-                                        .build()
-                        )
-                        .tableName("Table")
-                        .build()
-        )
     }
 
     @Test
